1e3e41c986d0a251333019af858fa9aec6be72a1
yucheng2 <17835204735@163.com>
2025-03-22 16:56:21 +0800

    refactor(配置): 移除开发服务器自动打开选项并更新html和服务器配置
    移除`dev`脚本中的`--open`选项，避免开发服务器自动打开浏览器。同时，更新`rsbuild.config.ts`中的`html`配置，添加页面标题，并设置服务器的`base`路径为`/antd-vue-file-uploader/`，以便更好地支持项目部署。

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginVue } from '@rsbuild/plugin-vue';
 
 export default defineConfig({
+  html: {
+    title: 'antd-vue-file-uploader',
+  },
   server: {
     port: 5000,
+    base: '/antd-vue-file-uploader/',
   },
   plugins: [pluginVue()],
 });
